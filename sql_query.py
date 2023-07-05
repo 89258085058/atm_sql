db153f6039af3461237136161b5bd69fa25aa87e
aleksandr.gorelov <aleksandr.gorelov@vk.team>
2023-07-05 17:21:38 +0300

    7.11

--- sql_query.py
+++ sql_query.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlite3
 import csv
+
 now_date = datetime.datetime.utcnow().strftime("%H:%M-%d.%m.%Y")
 
 
@@ -128,23 +129,36 @@ class SQL_atm:
     """Перевод денежных средств между пользователями"""
 
     @staticmethod
-    def transfer_money():
-        amount = input('Введите пожалуйста сумму которую желаете перевести: ')
-        while not amount.isdigit():
-            amount = input('Введите пожалуйста сумму которую желаете перевести: ')
-        number_card = input('Введите номер карты на которую необходимо выполнить перевод: ')
-        while not number_card.isdigit():
-            number_card = input('Введите пожалуйста сумму которую желаете перевести: ')
+    def transfer_money(num):
         with sqlite3.connect('atm.db') as db:
-            try:
-                cur = db.cursor()
-                cur.execute(f'''UPDATE Users_data SET Balance = Balance + {amount} WHERE Number_card = {number_card}''')
-                db.commit()
-                SQL_atm.info_balance(number_card)
-                return True
-            except:
-                print('Попытка выполнить некорректное действие')
-                return False
+            cur = db.cursor()
+            cur.execute(f'''SELECT Balance FROM Users_data WHERE Number_card = {num}''')
+            balance_info_result = cur.fetchone()
+            balance_card = balance_info_result[0]
+            print(balance_card)
+        amount = input('Введите пожалуйста сумму которую желаете перевести: ')
+        if int(amount) < int(balance_card):
+            while not amount.isdigit():
+                amount = input('Введите пожалуйста сумму которую желаете перевести: ')
+            number_card = input('Введите номер карты на которую необходимо выполнить перевод: ')
+            while not number_card.isdigit():
+                number_card = input('Введите пожалуйста сумму которую желаете перевести: ')
+            with sqlite3.connect('atm.db') as db:
+                try:
+                    cur = db.cursor()
+                    cur.execute(
+                        f'''UPDATE Users_data SET Balance = Balance + {amount} WHERE Number_card = {number_card}''')
+                    cur.execute(f'''UPDATE Users_data SET Balance = Balance - {amount} WHERE Number_card = {num}''')
+                    db.commit()
+                    SQL_atm.info_balance(number_card)
+                    SQL_atm.report_operation_1(now_date, number_card, "3", amount, "")
+                    SQL_atm.report_operation_2(now_date, "", "3", amount, number_card)
+                    return True
+                except:
+                    print('Попытка выполнить некорректное действие')
+                    return False
+        else:
+            print('У вас недостаточно средств')
 
     """Выбор операции"""
 
@@ -165,18 +179,16 @@ class SQL_atm:
                 SQL_atm.withdraw_money(number_card)
             elif operation == '3':
                 SQL_atm.depositing_money(number_card)
-            elif operation == '5':
-                SQL_atm.transfer_money()
             elif operation == '4':
                 print('Спасибо за ваш визит, всего доброго!')
                 return False
+            elif operation == '5':
+                SQL_atm.transfer_money(number_card)
             else:
                 print('Данная операция недоступна, приносим свои извинения')
 
-
     @staticmethod
     def report_operation_1(now_date, number_card, type_operation, amount, payee):
-
 
         user_data = [
             (now_date, number_card, type_operation, amount, payee)
@@ -187,7 +199,22 @@ class SQL_atm:
             writer.writerows(
                 user_data
             )
-        print("Данные внесены в отчет")
+        print("Данные внесены в отчет - report_1")
+
+    @staticmethod
+    def report_operation_2(Date, Payee, Type_operation, Amount, Sender):
+
+        user_data = [
+            (Date, Payee, Type_operation, Amount, Sender)
+        ]
+
+        with open("report_2.csv", "a", newline='') as file:
+            writer = csv.writer(file, delimiter=";")
+            writer.writerows(
+                user_data
+            )
+        print("Данные внесены в отчет - report_2")
+
 
 """
 Type_operation
@@ -197,4 +224,3 @@ Type_operation
 3 - Перевод денежных стредст
 
 """
-
